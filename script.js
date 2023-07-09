676c3c404cbc9581d27bb5c4d0ed23165a6a8cdf
박하영 <hayeong@baghayeong-ui-MacBook-Pro.local>
2023-07-09 18:28:45 +0900

    UPDATE: script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const todos = [
-    {id: 0, content: 'js 복습하기', done: false},
-    {id: 1, content: '개인 프로젝트', done: true},
+let todos = [
+    {id: 0, content: 'js 복습하기', done: false}, // 펄스 체크안됌
+    {id: 1, content: '개인 프로젝트', done: true},  // 트루 체크됌
     {id: 2, content: '알고리즘 문제 풀기', done: false}
   ]
   //    <li class="item-todo check-done">
@@ -12,8 +12,19 @@ const todos = [
   //         </div>
   //     </li>
   
+const $input = document.querySelector('.write-todolist')
+const $button = document.querySelector('.btn-send')
+const $btnDelete = document.querySelector('.btn-delete')
+//const $btnEdit = document.querySelector('.btn-edit')
+
+$button.addEventListener('click', add)
+$input.addEventListener('keyup', add)
+$btnDelete.addEventListener('click', erase)
+//$btnEdit.addEventListener('click',edit)
+
   function render() {
     const $todoList = document.querySelector('.list-todos')
+    $todoList.innerHTML = ''
     todos.forEach((v,i,a) => {
       const $todo = document.createElement('li')
       $todo.classList.add('item-todo')
@@ -24,10 +35,13 @@ const todos = [
       const $checkbox = document.createElement('input')
       $checkbox.type = 'checkbox'
       $checkbox.classList.add('checkbox')
+      $checkbox.checked = v.done
       
       // 컨텐트
       const $p = document.createElement('p')
       const $content = document.createTextNode(v.content)
+      if(v.done) $p.classList.add('line')
+
       $p.appendChild($content)
       // 버튼
       const $buttons = document.createElement('div')
@@ -47,5 +61,30 @@ const todos = [
       $todoList.appendChild($todo)
     })
   }
+
+  function add(e){
+    if(e.keyCode && e.keyCode !== 13) return
+    if(!$input.value.trim().length) return
+    // press enter or click add button
+
+    const id = todos[todos.length-1].id + 1
+    const content = $input.value.trim() // trim은 문자열 앞뒤에 있는 빈칸 뺌 ' 1234 ' => '1234'
+    const done = false
+    todos.push({id, content, done})
+    render()
+  }
+
+  function erase(e) {
+    const listItem = e.target.querySelector('.item-todo');
+    if (!listItem) return;
   
+    todos = todos.filter((v) => v !== listItem);
+    render();
+  }
+  
+
+  // function edit(){ // 인풋 따로 만들고 디자인 따로해야됌 css도 이벤트도 만들어줘야된다 체크박스 체크 될때 라인있고 체크해제하면 라인 삭제
+
+  // }
+
   render()
